d83b1a7397ecd44264d1152f94eeea09bfb48387
rishabh-sapkal <rishabhsapkal007@gmail.com>
2023-07-29 21:18:43 +0530

    contact form connected to Mail

--- src/app/mailer.service.ts
+++ src/app/mailer.service.ts
@@ -7,9 +7,21 @@ import emailjs from '@emailjs/browser';
 export class MailerService {
   constructor() {}
 
-  async send(details: any) {
+  async sendRideDetails(details: any) {
     emailjs.init('VROD8u7EmOdbSA23i');
     const status = await emailjs.send('service_jmac4tl', 'template_2dzftqa', {
+      from_name: 'Rishabh',
+      to_name: 'Team Innovative Web Tech',
+      reply_to: 'rishabhsapkal007@gmail.com',
+      ...details
+    });
+
+    return status
+  }
+
+  async sendContactDetails(details: any) {
+    emailjs.init('VROD8u7EmOdbSA23i');
+    const status = await emailjs.send('service_jmac4tl', 'template_x35kin4', {
       from_name: 'Rishabh',
       to_name: 'Team Innovative Web Tech',
       reply_to: 'rishabhsapkal007@gmail.com',
